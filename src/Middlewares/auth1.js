38133abbcdcf952571128c6081a5730c72d0a882
yuktasadana <yuktasadana216@gmail.com>
2022-11-08 21:41:02 -1100

    assignment of try and catch

---
+++ src/Middlewares/auth1.js
@@ -0,0 +1,13 @@
+/*const jwt= require('jsonwebtoken')
+const authorization = async function (req,res,next)
+
+{
+    const id = req.id
+    const userId = req.params.userId
+
+    if(userId!=id) return res.send({status:false,msg:"Unauthorised user"})
+
+    next()
+}
+
+module.exports.authorization=authorization*/
